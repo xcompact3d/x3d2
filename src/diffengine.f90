1fc57697bc00abd04681ada690ee36457682192c
Thibault Lestang <thibault.lestang@protonmail.com>
2023-02-26 10:49:31 +0000

    Reshuffle diffengine type a bit after initial draft, (nonfunctional)

--- src/diffengine.f90
+++ src/diffengine.f90
@@ -1,60 +1,68 @@
 module m_diffengine
-  use m_tridiagsolv
-  use m_stencil
+  use m_tridiagsolv, only: tridiagsolv, periodic_tridiagsolv
+  use m_stencil, only: stencil
   use m_stencil_definitions, only: get_boundary_stencils, get_stencil
 
   implicit none
 
-  type :: diffengine
+  type :: diffengine_t
      type(stencil) :: bulk_stencil
-     type(stencil), allocatable :: left_stencils(2)
-     type(stencil), allocatable :: right_stencils(2)
+     type(stencil) :: left_stencils(2)
+     type(stencil) :: right_stencils(2)
      class(tridiagsolv), allocatable :: tomsolv
-  end type diffengine
+   contains
+     procedure, public :: diff
+  end type diffengine_t
 
-  type(tri
+  interface diffengine_t
+     module procedure diffengine_constructor
+  end interface diffengine_t
 
 contains
 
-  function diffengine_constructor(bulk_key, n, order, left_key, right_key) &
+  function diffengine_constructor(bulk_key, length, order, left_key, right_key) &
        & result(diffengine)
     character(*), intent(in) :: bulk_key
     character(*), optional :: left_key, right_key
-    integer, intent(in) :: n, order
+    integer, intent(in) :: length, order
+    type(diffengine_t) :: diffengine
+    integer :: i, n
+    logical :: is_periodic
+    real :: lower_diag(length), upper_diag(length)
 
-    type(stencil) :: bulk_stencil
-    type(stencil) :: :: left_stencils(2), right_stencils(2)
 
+    diffengine%bulk_stencil = get_stencil(bulk_key, order)
 
-    diffengine%bulk_stencil = get_stencil(buld_key, order)
-
-    if (present(bulk_key) .and. present(right_key)) then
+    if (present(left_key) .and. present(right_key)) then
        is_periodic = .false.
        diffengine%left_stencils = get_boundary_stencils(left_key, order)
        diffengine%right_stencils = get_boundary_stencils(right_key, &
             & order, right=.true.)
     else if ((.not. present(left_key)) .and. &
-         &   (.not. present(right_key)) then
+         &   (.not. present(right_key))) then
        is_periodic = .true.
+       diffengine%left_stencils(:) = diffengine%bulk_stencil
+       diffengine%right_stencils(:) = diffengine%bulk_stencil
     else
        error stop "Both left and right boundary types must be specified."
     end if
 
     if (is_periodic) then
-       diffengine%tomsolv = periodic_tridiagsolv( &
-            & bulk_stencil%lower, bulk_stencil%upper, n)
+       upper_diag = [(diffengine%bulk_stencil%get_upper(), i = 1, length)]
+       lower_diag = [(diffengine%bulk_stencil%get_lower(), i = 1, length)]
+       diffengine%tomsolv = periodic_tridiagsolv(lower_diag, upper_diag)
     else
        upper_diag = [ &
-            & left_stencils%get_upper(), &
-            & (bulk_stencil%get_upper(), i= 3, n - 2), &
-            & reverse(right_stencils%get_upper()) &
+            & diffengine%left_stencils%get_upper(), &
+            & (diffengine%bulk_stencil%get_upper(), i= 3, length - 2), &
+            & reverse(diffengine%right_stencils%get_upper()) &
             ]
        lower_diag = [ &
-            & left_stencils%get_upper(), &
-            & (bulk_stencil%get_lower(), i = 3, n), &
-            & reverse(right_stencils%get_upper()) &
+            & diffengine%left_stencils%get_upper(), &
+            & (diffengine%bulk_stencil%get_lower(), i = 3, n), &
+            & reverse(diffengine%right_stencils%get_upper()) &
             ]
-       diffengine%tomsolv = tridiagsolv(lower_diag, upper_diag, n)
+       diffengine%tomsolv = tridiagsolv(lower_diag, upper_diag)
     end if
 
   end function diffengine_constructor
@@ -94,5 +102,11 @@ contains
 
     call self%tomsolv%solve(du, df)
   end subroutine diff
+
+  pure function reverse(x)
+    real, intent(in) :: x(:)
+    real, allocatable :: reverse(:)
+    reverse = x(size(x):1:-1)
+  end function reverse
 
 end module m_diffengine
