6d8a3156c4c8e89c9a33b6aed57364f167f75d76
Thibault Lestang <thibault.lestang@protonmail.com>
2023-02-28 16:58:25 +0000

    [doc] Add docstrings for components of allocator module

--- src/allocator.f90
+++ src/allocator.f90
@@ -2,8 +2,40 @@ module m_allocator
   implicit none
 
   type :: allocator_t
+     !! An instance of type allocator_t is responsible for the
+     !! maintenance of a linked list of instances of equal size
+     !! [[m_allocator(module):memblock_t(type)]] objects:
+     !!
+     !! ```
+     !!       ---- ---- ----     ---- ---- ----
+     !! ...-->|id=1|data|next|-->|id=0|data|next|-->null()
+     !!       ---- ---- ----     ---- ---- ----
+     !! ```
+     !!
+     !! the last block's `next` pointer being non associated.
+     !!
+     !! User code can request access to a memory block by using the
+     !! type bound procedure
+     !! [[m_allocator(module):get_block(function)]].  If the list is
+     !! not empty, a pointer to the first block on the list is
+     !! returned and the block is detached from the list.  If the list
+     !! is empty (i.e. all initially allocated blocks are currently
+     !! referenced to) then a new block is allocated before a pointer
+     !! to it is returned.
+     !!
+     !! In order to reuse memory it is important that user code
+     !! release blocks when they are not needed anymore.  This is done
+     !! by calling the type bound procedure
+     !! [[m_allocator(module):release_block(subroutine)]].  The
+     !! released block is then pushed in front of the block list.
+
      integer :: dims(3)
+     !> The id for the next allocated block.  This counter is
+     !> incremented each time a new block is allocated.
      integer :: next_id = 0
+     !> The pointer to the first block on the list.  Non associated if
+     !> the list is empty
+     ! TODO: Rename first to head
      class(memblock_t), pointer :: first => null()
    contains
      procedure :: get_block
@@ -14,10 +46,15 @@ module m_allocator
   end type allocator_t
 
   type :: memblock_t
+     !! Memory block type holding both a 3D data field and a pointer
+     !! to the next block.  The `memblock_t` type also holds a integer
+     !! `refcount` that counts the number of references to this
+     !! memblock.  User code is currently responsible for incrementing
+     !! the reference count.
      type(memblock_t), pointer :: next
      real, allocatable :: data(:, :, :)
      integer :: refcount = 0
-     integer :: id
+     integer :: id !! An integer identifying the memory block.
   end type memblock_t
 
   interface memblock_t
@@ -38,6 +75,8 @@ contains
   end function memblock_constructor
 
   function create_block(self, next) result(ptr)
+    !! Allocate memory for a new block and return a pointer to a new
+    !! [[m_allocator(module):memblock_t(type)]] object.
     class(allocator_t), intent(inout) :: self
     type(memblock_t), pointer, intent(in) :: next
     type(memblock_t), pointer :: newblock
@@ -49,28 +88,34 @@ contains
   end function create_block
 
   function get_block(self) result(handle)
-    !> Returns a pointer to the first available memory block, i.e. the
-    !> head of the free memory block list.
-    !> Example
-    !>     f%data => get_memory_block()
+    !! Return a pointer to the first available memory block, i.e. the
+    !! current head of the block list.  If the list is empty, allocate
+    !! a new block with [[m_allocator(module):create_block(function)]]
+    !! first.
+    !!
+    !! Example
+    !! ```
+    !! f%data => get_block()
+    !! ```
     class(allocator_t), intent(inout) :: self
     class(memblock_t), pointer :: handle
     class(memblock_t), allocatable, target :: ptr
-    ! If we're about to allocate the last block, extend the pool
-    ! first.
+    ! If the list is empty, allocate a new block before returning a
+    ! pointer to it.
     if(.not. associated(self%first)) then
-       !> Construct a memblock_t. This effectively allocates
-       !> storage space.
+       ! Construct a memblock_t. This effectively allocates
+       ! storage space.
        self%first => self%create_block(next=self%first)
     end if
     handle => self%first
-    self%first => self%first%next
-    handle%next => null()
+    self%first => self%first%next ! 2nd block becomes head block
+    handle%next => null() ! Detach ex-head block from the block list
   end function get_block
 
   subroutine release_block(self, handle)
-    !> Release memory block pointed to by handle to the pool.  It is
-    !> pushed to the front of the free memory block list.
+    !! Release memory block pointed to by HANDLE to the block list.
+    !! It is pushed to the front of the block list, in other words it
+    !! is made the head block.
     class(allocator_t), intent(inout) :: self
     class(memblock_t), pointer :: handle
     handle%next => self%first
@@ -78,10 +123,11 @@ contains
   end subroutine release_block
 
   subroutine destroy(self)
-    !> Destroy each memory block in the free memory block list, from
-    !> head to tail.  Deallocation of a memblock_t triggers all
-    !> final (see def of memblock_t type) procedures and therefore
-    !> deallocation of the block's storage space.
+    !! Go through the block list from head to tail, deallocating each
+    !! memory block in turn.  Deallocation of a
+    !! [[m_allocator(module):memblock_t(type)]] object automatically
+    !! deallocates its internal allocatable
+    !! [[memblock_t(type):data(variable)]] array.
     class(allocator_t), intent(inout) :: self
     type(memblock_t), pointer :: current
     do
@@ -94,6 +140,11 @@ contains
   end subroutine destroy
 
   function get_block_ids(self)
+    !! Utility function that returns a array made of the `id` of the
+    !! block currently in the block list.  Return the array [0] if
+    !! block list is empty.
+    ! TODO: Block indices should start at 1 or return [-1] in case of
+    ! empty block list.
     class(allocator_t), intent(inout) :: self
     integer, allocatable :: get_block_ids(:)
     class(memblock_t), pointer :: current
