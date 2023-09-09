5290fa4447db37fd2f6db663d77845e837801c52
Semih Akkurt <s.akkurt18@imperial.ac.uk>
2023-09-09 14:43:41 +0100

    feat(cuda): Add a distributed tridiagonal solver.

---
+++ src/common.f90
@@ -0,0 +1,7 @@
+module m_common
+   implicit none
+
+   integer, parameter :: dp=kind(0.0d0), SZ=32
+   real(dp), parameter :: pi = 4*atan(1.0_dp)
+
+end module m_common
---
+++ src/cuda/kernels_dist.f90
@@ -0,0 +1,153 @@
+module m_cuda_kernels_dist
+   use cudafor
+
+   use m_common, only: dp
+
+   implicit none
+
+contains
+
+   attributes(global) subroutine der_univ_dist( &
+      du, send_u_b, send_u_e, u, u_b, u_e, coeffs_b, coeffs_e, coeffs, n, &
+      alfa, ffr, fbc &
+      )
+      implicit none
+
+      ! Arguments
+      real(dp), device, intent(out), dimension(:, :, :) :: du, send_u_b, &
+                                                           send_u_e
+      real(dp), device, intent(in), dimension(:, :, :) :: u, u_b, u_e
+      real(dp), device, intent(in), dimension(:) :: ffr, fbc
+      real(dp), device, intent(in), dimension(:, :) :: coeffs_b, coeffs_e
+      real(dp), device, intent(in), dimension(:) :: coeffs
+      real(dp), value, intent(in) :: alfa
+      integer, value, intent(in) :: n
+
+      ! Local variables
+      integer :: i, j, b, k, lj
+      integer :: jm2, jm1, jp1, jp2
+      integer :: n_s, n_m, n_b, n_e !stencil, middle, begin, end
+
+      real(dp) :: temp_du, c_m4, c_m3, c_m2, c_m1, c_j, c_p1, c_p2, c_p3, c_p4
+
+      i = threadIdx%x
+      b = blockIdx%x
+
+      n_s = (size(coeffs)-1)/2
+      n_m = size(coeffs)
+      n_b = size(coeffs_b, dim=2)
+      n_e = size(coeffs_e, dim=2)
+
+      ! store bulk coeffs in the registers
+      c_m4 = coeffs(1); c_m3 = coeffs(2); c_m2 = coeffs(3); c_m1 = coeffs(4)
+      c_j = coeffs(5)
+      c_p1 = coeffs(6); c_p2 = coeffs(7); c_p3 = coeffs(8); c_p4 = coeffs(9)
+
+      du(i, 1, b) = coeffs(1)*u_b(i, 1, b) + coeffs(2)*u_b(i, 2, b) &
+                    + coeffs(3)*u_b(i, 3, b) + coeffs(4)*u_b(i, 4, b) &
+                    + coeffs(5)*u(i, 1, b) &
+                    + coeffs(6)*u(i, 2, b) + coeffs(7)*u(i, 3, b) &
+                    + coeffs(8)*u(i, 4, b) + coeffs(9)*u(i, 5, b)
+      du(i, 2, b) = coeffs(1)*u_b(i, 2, b) + coeffs(2)*u_b(i, 3, b) &
+                    + coeffs(3)*u_b(i, 4, b) + coeffs(4)*u(i, 1, b) &
+                    + coeffs(5)*u(i, 2, b) &
+                    + coeffs(6)*u(i, 3, b) + coeffs(7)*u(i, 4, b) &
+                    + coeffs(8)*u(i, 5, b) + coeffs(9)*u(i, 6, b)
+      du(i, 3, b) = coeffs(1)*u_b(i, 3, b) + coeffs(2)*u_b(i, 4, b) &
+                    + coeffs(3)*u(i, 1, b) + coeffs(4)*u(i, 2, b) &
+                    + coeffs(5)*u(i, 3, b) &
+                    + coeffs(6)*u(i, 4, b) + coeffs(7)*u(i, 5, b) &
+                    + coeffs(8)*u(i, 6, b) + coeffs(9)*u(i, 7, b)
+      du(i, 3, b) = ffr(3)*(du(i, 3, b) - alfa*du(i, 2, b))
+      du(i, 4, b) = coeffs(1)*u_b(i, 4, b) + coeffs(2)*u(i, 1, b) &
+                    + coeffs(3)*u(i, 2, b) + coeffs(4)*u(i, 3, b) &
+                    + coeffs(5)*u(i, 4, b) &
+                    + coeffs(6)*u(i, 5, b) + coeffs(7)*u(i, 6, b) &
+                    + coeffs(8)*u(i, 7, b) + coeffs(9)*u(i, 8, b)
+      du(i, 4, b) = ffr(4)*(du(i, 4, b) - alfa*du(i, 3, b))
+
+      do j = n_s+1, n-n_s
+         temp_du = c_m4*u(i, j-4, b) + c_m3*u(i, j-3, b) &
+                 + c_m2*u(i, j-2, b) + c_m1*u(i, j-1, b) &
+                 + c_j*u(i, j, b) &
+                 + c_p1*u(i, j+1, b) + c_p2*u(i, j+2, b) &
+                 + c_p3*u(i, j+3, b) + c_p4*u(i, j+4, b)
+         du(i, j, b) = ffr(j)*(temp_du - alfa*du(i, j-1, b))
+      end do
+
+      j = n-3
+      du(i, j, b) = coeffs(1)*u(i, j-4, b) + coeffs(2)*u(i, j-3, b) &
+                  + coeffs(3)*u(i, j-2, b) + coeffs(4)*u(i, j-1, b) &
+                  + coeffs(5)*u(i, j, b) &
+                  + coeffs(6)*u(i, j+1, b) + coeffs(7)*u(i, j+2, b) &
+                  + coeffs(8)*u(i, j+3, b) + coeffs(9)*u_e(i, 1, b)
+      du(i, j, b) = ffr(j)*(du(i, j, b) - alfa*du(i, j-1, b))
+      j = n-2
+      du(i, j, b) = coeffs(1)*u(i, j-4, b) + coeffs(2)*u(i, j-3, b) &
+                    + coeffs(3)*u(i, j-2, b) + coeffs(4)*u(i, j-1, b) &
+                    + coeffs(5)*u(i, j, b) &
+                    + coeffs(6)*u(i, j+1, b) + coeffs(7)*u(i, j+2, b) &
+                    + coeffs(8)*u_e(i, 1, b) + coeffs(9)*u_e(i, 2, b)
+      du(i, j, b) = ffr(j)*(du(i, j, b) - alfa*du(i, j-1, b))
+      j = n-1
+      du(i, j, b) = coeffs(1)*u(i, j-4, b) + coeffs(2)*u(i, j-3, b) &
+                    + coeffs(3)*u(i, j-2, b) + coeffs(4)*u(i, j-1, b) &
+                    + coeffs(5)*u(i, j, b) &
+                    + coeffs(6)*u(i, j+1, b) + coeffs(7)*u_e(i, 1, b) &
+                    + coeffs(8)*u_e(i, 2, b) + coeffs(9)*u_e(i, 3, b)
+      du(i, j, b) = ffr(j)*(du(i, j, b) - alfa*du(i, j-1, b))
+      j = n
+      du(i, j, b) = coeffs(1)*u(i, j-4, b) + coeffs(2)*u(i, j-3, b) &
+                    + coeffs(3)*u(i, j-2, b) + coeffs(4)*u(i, j-1, b) &
+                    + coeffs(5)*u(i, j, b) &
+                    + coeffs(6)*u_e(i, 1, b) + coeffs(7)*u_e(i, 2, b) &
+                    + coeffs(8)*u_e(i, 3, b) + coeffs(9)*u_e(i, 4, b)
+      du(i, j, b) = ffr(j)*(du(i, j, b) - alfa*du(i, j-1, b))
+
+      send_u_e(i, 1, b) = du(i, n, b)
+
+      ! Backward pass of the hybrid algorithm
+      do j = n - 2, 2, -1
+         du(i, j, b) = du(i, j, b) - fbc(j)*du(i, j + 1, b)
+      end do
+      du(i, 1, b) = ffr(1)*(du(i, 1, b) - fbc(1)*du(i, 2, b))
+      send_u_b(i, 1, b) = du(i, 1, b)
+
+   end subroutine der_univ_dist
+
+   attributes(global) subroutine der_univ_subs(du, recv_u_b, recv_u_e, &
+                                               n, alfa, dist_sa, dist_sc)
+      implicit none
+
+      ! Arguments
+      real(dp), device, intent(out), dimension(:, :, :) :: du
+      real(dp), device, intent(in), dimension(:, :, :) :: recv_u_b, recv_u_e
+      real(dp), device, intent(in), dimension(:) :: dist_sa, dist_sc
+      real(dp), value, intent(in) :: alfa
+      integer, value, intent(in) :: n
+
+      ! Local variables
+      integer :: i, j, b
+      real(dp) :: ur, bl, recp, du_1, du_n
+
+      i = threadIdx%x
+      b = blockIdx%x
+
+      bl = dist_sa(1)
+      ur = dist_sc(n)
+      recp = 1._dp/(1._dp - ur*bl)
+
+      !du(i, 1, b) = recp*(du(i, 1, b) - bl*recv_u_b(i, 1, b))
+      !du(i, n, b) = recp*(du(i, n, b) - ur*recv_u_e(i, 1, b))
+      du_1 = recp*(du(i, 1, b) - bl*recv_u_b(i, 1, b))
+      du_n = recp*(du(i, n, b) - ur*recv_u_e(i, 1, b))
+
+      du(i, 1, b) = du_1
+      do j = 2, n-1
+         du(i, j, b) = (du(i, j, b) - dist_sa(j)*du_1 - dist_sc(j)*du_n)
+      end do
+      du(i, n, b) = du_n
+
+   end subroutine der_univ_subs
+
+end module m_cuda_kernels_dist
