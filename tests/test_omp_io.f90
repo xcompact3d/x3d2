47496f5c2fa9fa9f9296de5c1bad894d6852be55
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2024-04-08 13:54:59 +0100

    Refactor ADIOS IO functionality into type

--- tests/test_omp_io.f90
+++ tests/test_omp_io.f90
@@ -1,7 +1,140 @@
+
+module adios_io
+   use adios2
+   use mpi
+   use m_allocator, only: field_t
+
+   implicit none
+
+   type :: adios_io_t
+     type(adios2_adios) :: adios_ctx
+     integer :: irank, nproc
+  contains
+    procedure:: init 
+    procedure:: deinit 
+    procedure:: handle_fatal_error 
+    procedure:: read 
+    procedure:: write 
+  end type
+
+  contains
+     subroutine init(self)
+       class(adios_io_t), intent(inout) :: self
+
+       integer :: ierr
+
+       call MPI_Comm_rank(MPI_COMM_WORLD, self%irank, ierr)
+
+       call adios2_init(self%adios_ctx, MPI_COMM_WORLD, ierr)
+       if (.not.self%adios_ctx%valid) then
+         print*, "ADIOS2 Error code:", ierr
+         call self%handle_fatal_error("Cannot initialise ADIOS context.")
+       endif
+     end subroutine
+
+     subroutine deinit(self)
+       class(adios_io_t), intent(inout) :: self
+       integer :: ierr
+       if (self%adios_ctx%valid) then
+         call adios2_finalize(self%adios_ctx, ierr)
+       endif
+     end subroutine
+
+     subroutine write(self, in_arr, fpath, varname, icount, ishape, istart)
+       class(adios_io_t), intent(inout) :: self
+       class(field_t), pointer, intent(in) :: in_arr !! Field to be outputted
+       character(*), intent(in) :: fpath !! Path to ouptut file
+       character(*), intent(in) :: varname !! Name of variable in output file
+     integer(kind=8), dimension(3), intent(in) :: icount !! Global size of in_arr
+     integer(kind=8), dimension(3), intent(in) :: ishape !! Local size of in_arr
+     integer(kind=8), dimension(3), intent(in) :: istart !! Local offset of in_arr
+
+       type(adios2_io) :: io
+       type(adios2_engine) :: writer
+       type(adios2_variable) :: adios_var
+       integer :: ierr
+
+       call adios2_declare_io (io, self%adios_ctx, 'write', ierr)
+       if (.not.io%valid) then
+          call self%handle_fatal_error("Cannot create ADIOS2 IO")
+       endif
+
+       call adios2_open(writer, io, fpath, adios2_mode_write, ierr)
+       if (.not.writer%valid) then
+          call self%handle_fatal_error("Cannot create ADIOS2 writer")
+       endif
+
+       ! TODO tie double precision variable to array
+       call adios2_define_variable(adios_var, io, varname, adios2_type_dp, &
+                                   3, ishape, istart, icount, adios2_constant_dims, ierr)
+
+       call adios2_begin_step(writer, adios2_step_mode_append, ierr)
+       call adios2_put(writer, adios_var, in_arr%data, ierr)
+       call adios2_end_step(writer, ierr)
+
+       if (writer%valid) then
+          call adios2_close(writer, ierr)
+       endif
+
+     end subroutine
+
+     subroutine read(self, out_arr, fpath, varname)
+       class(adios_io_t), intent(inout) :: self
+        class(field_t), pointer, intent(in) :: out_arr !! Field to be read from file
+        character(*), intent(in) :: fpath !! Path to input file
+        character(*), intent(in) :: varname !! Name of variable in input file
+
+        integer(8), parameter :: initial_step = 0
+        integer(8), parameter :: n_steps = 1
+
+       type(adios2_io) :: io
+       type(adios2_engine) :: reader
+       type(adios2_variable) :: adios_var
+       integer :: ierr
+
+       if (self%irank /= 0) then
+         return
+       endif
+
+       call adios2_declare_io (io, self%adios_ctx, 'read', ierr)
+       if (.not.io%valid) then
+          call self%handle_fatal_error("Cannot create ADIOS2 IO")
+       endif
+
+       call adios2_open(reader, io, fpath, adios2_mode_read, MPI_COMM_SELF, ierr)
+       if (.not.reader%valid) then
+          call self%handle_fatal_error("Cannot create ADIOS2 reader")
+       endif
+
+       call adios2_begin_step(reader, adios2_step_mode_read, ierr)
+       call adios2_inquire_variable(adios_var, io, varname, ierr)
+       if (.not.adios_var%valid) then
+          call self%handle_fatal_error("Cannot fetch ADIOS2 IO")
+       endif
+
+       call adios2_set_step_selection(adios_var, initial_step, n_steps, ierr)
+       call adios2_get(reader, adios_var, out_arr%data, ierr)
+       call adios2_end_step(reader, ierr)
+
+       if (reader%valid) then
+          call adios2_close(reader, ierr)
+       endif
+     end subroutine
+
+     subroutine handle_fatal_error(self, msg)
+       class(adios_io_t), intent(in) :: self
+       character(*), intent(in) :: msg
+
+       error stop msg
+     end subroutine
+end module
+
+
 program test_omp_io
    use iso_fortran_env, only: stderr => error_unit
    use adios2
    use mpi
+   use adios_io
 
    use m_allocator, only: allocator_t, field_t
    use m_common, only: dp, DIR_C
@@ -19,9 +152,9 @@ program test_omp_io
    !> Error code, used for MPI and ADIOS
    integer :: ierr
 
-   type(adios2_adios) :: adios_ctx
-
    type(allocator_t), target :: omp_allocator
+
+   type(adios_io_t) :: io
 
    integer(kind=8), dimension(3) :: ishape, istart, icount
    integer :: n, n_block, n_glob
@@ -33,7 +166,11 @@ program test_omp_io
 
    allpass = .true.
 
-   call init_mpi_adios()
+   call MPI_Init(ierr)
+   call MPI_Comm_rank(MPI_COMM_WORLD, irank, ierr)
+   call MPI_Comm_size(MPI_COMM_WORLD, nproc, ierr)
+
+   call io%init()
 
    ! if (nrank == 0 && nproc > 1) print*, 'Parallel run with', nproc, 'ranks'
    if (nproc > 1) call abort_test("Test does not support multiple MPI processes")
@@ -43,8 +180,8 @@ program test_omp_io
    !================
 
    icount = (/ nx, ny, nz/) ! global size
-   istart = (/ 0, 0, 0/) ! local offset
    ishape = (/ nx, ny, nz/) ! local size
+   istart = (/ 0, 0, 0/) ! local offset
 
    omp_allocator = allocator_t(nx, ny, nz, SZ)
    print*, 'OpenMP allocator instantiated'
@@ -60,11 +197,11 @@ program test_omp_io
       end do
    end do
 
-   call write_test_file(arr_to_write, "__FILE__.bp",  "TestArr")
+   call io%write(arr_to_write, "__FILE__.bp",  "TestArr", icount, ishape, istart)
    call omp_allocator%release_block(arr_to_write)
 
    arr_to_read => omp_allocator%get_block(DIR_C)
-   call read_test_file(arr_to_read, "__FILE__.bp", "TestArr")
+   call io%read(arr_to_read, "__FILE__.bp", "TestArr")
 
    do k = 1, nz
       do j = 1, ny
@@ -88,107 +225,21 @@ program test_omp_io
       error stop 'SOME TESTS FAILED.'
    end if
 
-   call deinit_mpi_adios()
+   call io%deinit()
+   call MPI_Finalize(ierr)
 
    contains
 
-     subroutine init_mpi_adios()
-       call MPI_Init(ierr)
-       call MPI_Comm_rank(MPI_COMM_WORLD, irank, ierr)
-       call MPI_Comm_size(MPI_COMM_WORLD, nproc, ierr)
-
-       call adios2_init(adios_ctx, MPI_COMM_WORLD, ierr)
-       if (.not.adios_ctx%valid) then
-          call abort_test("Cannot create ADIOS2 context")
-       endif
-     end subroutine
-
-     subroutine deinit_mpi_adios()
-       if (adios_ctx%valid) then
-         call adios2_finalize(adios_ctx, ierr)
-       endif
-
-       call MPI_Finalize(ierr)
-     end subroutine
-
      subroutine abort_test(msg)
-       character(*) :: msg
+       character(*), intent(in) :: msg
+       integer :: ierr
 
-       call deinit_mpi_adios()
+       call io%deinit()
+       call MPI_Finalize(ierr)
 
        error stop msg
      end subroutine
 
-     subroutine write_test_file(in_arr, fname, varname)
-       class(field_t), pointer, intent(in) :: in_arr !! array to save & restore
-       character(*), intent(in) :: fname
-       character(*), intent(in) :: varname
 
-       type(adios2_io) :: io
-       type(adios2_engine) :: engine
-       type(adios2_variable) :: adios_var
-       integer :: ierr
-
-       call adios2_declare_io (io, adios_ctx, 'TestWrite', ierr)
-       if (.not.io%valid) then
-          call abort_test("Cannot create ADIOS2 IO")
-       endif
-
-       call adios2_open(engine, io, fname, adios2_mode_write, ierr)
-       if (.not.engine%valid) then
-          call abort_test("Cannot create ADIOS2 engine")
-       endif
-
-       ! TODO tie double precision variable to array
-       call adios2_define_variable(adios_var, io, varname, adios2_type_dp, &
-                                   3, ishape, istart, icount, adios2_constant_dims, ierr)
-
-       call adios2_begin_step(engine, adios2_step_mode_append, ierr)
-       call adios2_put(engine, adios_var, arr_to_write%data, ierr)
-       call adios2_end_step(engine, ierr)
-
-       if (engine%valid) then
-          call adios2_close(engine, ierr)
-       endif
-
-     end subroutine
-
-     subroutine read_test_file(out_arr, fname, varname)
-       class(field_t), pointer, intent(out) :: out_arr !! array to save & restore
-       character(*), intent(in) :: fname
-       character(*), intent(in) :: varname
-
-       type(adios2_io) :: io
-       type(adios2_engine) :: engine
-       type(adios2_variable) :: adios_var
-       integer :: ierr
-
-       if (irank /= 0) then
-         return
-       endif
-
-       call adios2_declare_io (io, adios_ctx, 'TestRead', ierr)
-       if (.not.io%valid) then
-          call abort_test("Cannot create ADIOS2 IO")
-       endif
-
-       call adios2_open(engine, io, fname, adios2_mode_read, MPI_COMM_SELF, ierr)
-       if (.not.engine%valid) then
-          call abort_test("Cannot create ADIOS2 engine")
-       endif
-
-       call adios2_begin_step(engine, adios2_step_mode_read, ierr)
-       call adios2_inquire_variable(adios_var, io, varname, ierr)
-       if (.not.adios_var%valid) then
-          call abort_test("Cannot fetch ADIOS2 IO")
-       endif
-
-       call adios2_set_step_selection(adios_var, 0_8, 1_8, ierr)
-       call adios2_get(engine, adios_var, out_arr%data, ierr)
-       call adios2_end_step(engine, ierr)
-
-       if (engine%valid) then
-          call adios2_close(engine, ierr)
-       endif
-     end subroutine
 end program
+
