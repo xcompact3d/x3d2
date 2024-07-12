00b4dab84ab6254a2561c68a3a0cc52c2dd83fc2
Semih Akkurt <s.akkurt18@imperial.ac.uk>
2024-07-12 15:20:15 +0100

    Add a test for non-periodic thomas implementation.

--- tests/cuda/test_thom.f90
+++ tests/cuda/test_thom.f90
@@ -15,9 +15,8 @@ program test_thom
 
   type(cuda_tdsops_t) :: tdsops
 
-  integer :: n, n_block, i, j, k, n_iters
+  integer :: n, n_block, i, j, k, n_iters, ndof
   integer :: n_glob
-  integer :: nrank, nproc
   integer :: ierr, ndevs, devnum, memClockRt, memBusWidth
 
   type(dim3) :: blocks, threads
@@ -28,6 +27,7 @@ program test_thom
   n = n_glob
   n_block = 512*512/SZ
   n_iters = 100
+  ndof = n_glob*n_block*SZ
 
   allocate (u(SZ, n, n_block), du(SZ, n, n_block))
   allocate (u_dev(SZ, n, n_block), du_dev(SZ, n, n_block))
@@ -38,7 +38,7 @@ program test_thom
   do k = 1, n_block
     do j = 1, n
       do i = 1, SZ
-        u(i, j, k) = sin((j - 1 + nrank*n)*dx_per)
+        u(i, j, k) = sin((j - 1)*dx_per)
       end do
     end do
   end do
@@ -59,25 +59,10 @@ program test_thom
     call exec_thom_tds_compact(du_dev, u_dev, tdsops, blocks, threads)
   end do
   call cpu_time(tend)
-  if (nrank == 0) print *, 'Total time', tend - tstart
+  print *, 'Total time', tend - tstart
 
-  ! BW utilisation and performance checks
   ! 2 in fw pass, 2 in bw pass, 2 in final periodic pass: 6 in total
-  achievedBW = 6._dp*n_iters*n*n_block*SZ*dp/(tend - tstart)
-
-  if (nrank == 0) then
-    print'(a, f8.3, a)', 'Achieved BW: ', achievedBW/2**30, ' GiB/s'
-  end if
-
-  ierr = cudaDeviceGetAttribute(memClockRt, cudaDevAttrMemoryClockRate, 0)
-  ierr = cudaDeviceGetAttribute(memBusWidth, &
-                                cudaDevAttrGlobalMemoryBusWidth, 0)
-  deviceBW = 2*memBusWidth/8._dp*memClockRt*1000
-
-  if (nrank == 0) then
-    print'(a, f8.3, a)', 'Device BW:   ', deviceBW/2**30, ' GiB/s'
-    print'(a, f5.2)', 'Effective BW util min: %', achievedBW/deviceBW*100
-  end if
+  call checkperf(tend - tstart, n_iters, ndof, 6._dp)
 
   ! check error
   du = du_dev
@@ -85,22 +70,85 @@ program test_thom
   norm_du = norm_du*norm_du/n_glob/n_block/SZ
   norm_du = sqrt(norm_du)
 
-  if (nrank == 0) print *, 'error norm', norm_du
+  print *, 'error norm', norm_du
 
-  if (nrank == 0) then
-    if (norm_du > tol) then
-      allpass = .false.
-      write (stderr, '(a)') 'Check second derivatives... failed'
-    else
-      write (stderr, '(a)') 'Check second derivatives... passed'
-    end if
+  if (norm_du > tol) then
+    allpass = .false.
+    write (stderr, '(a)') 'Check periodic second derivatives... failed'
+  else
+    write (stderr, '(a)') 'Check periodic second derivatives... passed'
   end if
 
+  do k = 1, n_block
+    do j = 1, n
+      do i = 1, SZ
+        u(i, j, k) = sin((j - 1)*dx)
+      end do
+    end do
+  end do
+
+  ! move data to device
+  u_dev = u
+
+  ! preprocess the operator and coefficient arrays
+  tdsops = cuda_tdsops_init(n, dx, operation='second-deriv', &
+                            scheme='compact6', &
+                            bc_start='dirichlet', bc_end='dirichlet')
+  call cpu_time(tstart)
+  do i = 1, n_iters
+    call exec_thom_tds_compact(du_dev, u_dev, tdsops, blocks, threads)
+  end do
+  call cpu_time(tend)
+  print *, 'Total time', tend - tstart
+
+  ! 2 in fw pass, 2 in bw pass: 4 in total
+  call checkperf(tend - tstart, n_iters, ndof, 4._dp)
+
+  ! check error
+  du = du_dev
+  norm_du = norm2(u + du)
+  norm_du = norm_du*norm_du/n_glob/n_block/SZ
+  norm_du = sqrt(norm_du)
+
+  print *, 'error norm', norm_du
+
+  if (norm_du > tol) then
+    allpass = .false.
+    write (stderr, '(a)') 'Check dirichlet second derivatives... failed'
+  else
+    write (stderr, '(a)') 'Check dirichlet second derivatives... passed'
+  end if
   if (allpass) then
-    if (nrank == 0) write (stderr, '(a)') 'ALL TESTS PASSED SUCCESSFULLY.'
+    write (stderr, '(a)') 'ALL TESTS PASSED SUCCESSFULLY.'
   else
     error stop 'SOME TESTS FAILED.'
   end if
+
+contains
+
+  subroutine checkperf(t_tot, n_iters, ndof, consumed_bw)
+    implicit none
+
+    real(dp), intent(in) :: t_tot, consumed_bw
+    integer, intent(in) :: n_iters, ndof
+
+    real(dp) :: achievedBW, devBW
+    integer :: ierr, memClockRt, memBusWidth
+
+    ! BW utilisation and performance checks
+    achievedBW = consumed_bw*n_iters*ndof*dp/t_tot
+
+    print'(a, f8.3, a)', 'Achieved BW: ', achievedBW/2**30, ' GiB/s'
+
+    ierr = cudaDeviceGetAttribute(memClockRt, cudaDevAttrMemoryClockRate, 0)
+    ierr = cudaDeviceGetAttribute(memBusWidth, &
+                                  cudaDevAttrGlobalMemoryBusWidth, 0)
+    devBW = 2*memBusWidth/8._dp*memClockRt*1000
+
+    print'(a, f8.3, a)', 'Device BW:   ', devBW/2**30, ' GiB/s'
+    print'(a, f5.2)', 'Effective BW util: %', achievedBW/devBW*100
+
+  end subroutine checkperf
 
 end program test_thom
 
