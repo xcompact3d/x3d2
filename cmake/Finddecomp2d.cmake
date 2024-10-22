c0649d216821ddbb6d8430623cd69e07c166ef7c
Jacques Xing <jxing1@typhoon.ae.ic.ac.uk>
2024-10-22 13:36:03 +0100

    Add provision to use pre-built decomp2d library using cmake .. -Ddecomp2d_INCLUDE_DIRS=/path-to-2decomp-fft/build

--- cmake/Finddecomp2d.cmake
+++ cmake/Finddecomp2d.cmake
@@ -1,6 +1,6 @@
 # - Find the 2decomp-fft library
 find_package(decomp2d
-             PATHS ${CMAKE_SOURCE_DIR}/decomp2d/build)
+             PATHS ${CMAKE_SOURCE_DIR}/decomp2d/build ${decomp2d_INCLUDE_DIRS})
 if (decomp2d_FOUND)
   message(STATUS "2decomp-fft FOUND")
 else(decomp2d_FOUND)
