0d0446792aa70b50add1146b14384f143fa3828c
Irufan Ahmed <ia267@users.noreply.github.com>
2025-01-24 17:54:15 +0000

    Changed html theme

--- docs/ford.md
+++ docs/ford.md
@@ -2,14 +2,13 @@
 project: x3d2
 src_dir: ../src
 summary: 
-x3d2 is the current codename for what is to become Xcompact3d
+"x3d2 is the current codename for what is to become Xcompact3d
 version 2.0.  This project is a rewrite of Xcompact3d leading to
 
 - Improved serial and parallel performance.
 - Support for NVIDIA GPU accelerators (using CUDA Fortran).
-- Improved modularity and testability.
+- Improved modularity and testability."
 author: Xcompact3d Team
-exclude: 
 search: true
 creation_date: true
 coloured_edges: true
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -68,9 +68,23 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_material"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ["_static"]
+
+html_theme_options = {
+        'repo_url': 'https://github.com/xcompact3d/x3d2',
+        'repo_name': 'X3D2',
+        'html_minify': True,
+        'css_minify': True,
+        'nav_title': 'X3D2',
+        'globaltoc_depth' : 2
+
+}
+
+html_sidebars = {
+    "**": ["logo-text.html", "globaltoc.html", "localtoc.html", "searchbox.html"]
+}
