5745f6df79c60ddf3f5124439c4597e5defafcd7
Irufan Ahmed <ia267@users.noreply.github.com>
2025-02-15 00:31:05 +0000

    refactored test case following adios2 base class

--- tests/test_adios2_read_write.f90
+++ tests/test_adios2_read_write.f90
@@ -1,121 +1,100 @@
 program test_adios2
-   use mpi
-   use adios2
-   use m_common, only: dp
-   use iso_fortran_env, only: int64, stderr => error_unit
+    use mpi
+    use m_base_adios2
+    use m_adios2_writer
+    use m_adios2_reader
+    use m_common, only: dp
+    use iso_fortran_env, only: int64, real64, stderr => error_unit
+    implicit none
 
-   implicit none
+    ! ADIOS2 handlers
+    type(adios2_writer_t) :: writer
+    type(adios2_reader_t) :: reader
 
-   ! adios2 handlers
-   type(adios2_adios) :: adios
-   type(adios2_io)    :: io_write, io_read
-   type(adios2_variable) :: var
-   type(adios2_engine) :: engine
+    ! MPI variables
+    integer :: ierr, irank, isize
+    integer(kind=int64), dimension(2) :: shape_dims, start_dims, count_dims
+    integer(kind=int64), dimension(2) :: sel_start, sel_count
+    real(kind=real64), dimension(:,:), allocatable :: data_write, data_read
 
-   ! MPI variables
-   integer :: ierr, irank, isize
-   integer(kind=int64), dimension(2) :: shape_dims, start_dims, count_dims
-   integer(kind=int64), dimension(2) :: sel_start, sel_count
-   real(dp), dimension(:, :), allocatable :: data_write, data_read
+    ! application variables
+    integer :: i, j, rank_id, inx = 3, iny = 4
+    logical :: allpass = .true.
+    real(kind=real64) :: expected
 
-   ! application variables
-   integer :: i, j, inx = 3, iny = 4
-   logical :: allpass = .true.
+    ! launch MPI
+    call MPI_Init(ierr)
+    call MPI_Comm_rank(MPI_COMM_WORLD, irank, ierr)
+    call MPI_Comm_size(MPI_COMM_WORLD, isize, ierr)
 
-   ! launch MPI
-   call MPI_Init(ierr)
-   call MPI_Comm_rank(MPI_COMM_WORLD, irank, ierr)
-   call MPI_Comm_size(MPI_COMM_WORLD, isize, ierr)
+    ! data initialization
+    allocate(data_write(inx, iny))
 
-   ! data initialisation
-   allocate (data_write(inx, iny))
+    ! initialize data (unique per rank)
+    do j = 1, iny
+        do i = 1, inx
+            data_write(i,j) = irank*inx*iny + (j-1)*inx + (i-1)
+        end do
+    end do
 
-   ! initialize data (unique per rank)
-   do j = 1, iny
-      do i = 1, inx
-         data_write(i, j) = irank*inx*iny + (j - 1)*inx + (i - 1)
-      end do
-   end do
+    ! global shape and local offsets for parallel I/O
+    shape_dims = [isize*inx, iny]
+    start_dims = [irank*inx, 0]
+    count_dims = [inx, iny]
 
-   ! global shape and local offsets for parallel I/O
-   shape_dims = [isize*inx, iny]
-   start_dims = [irank*inx, 0]
-   count_dims = [inx, iny]
+    ! write data
+    call writer%init_adios2(MPI_COMM_WORLD, "test_io_write")
+    call writer%open_adios2("test_output.bp", adios2_mode_write)
+    call writer%begin_step_adios2()
+    call writer%write_data("data2D", data_write, shape_dims, start_dims, count_dims)
+    call writer%end_step_adios2()
+    call writer%close_adios2()
 
-   ! create adios handler passing the communicator and error flag
-   call adios2_init(adios, MPI_COMM_WORLD, ierr)
+    if (allocated(data_write)) deallocate(data_write)
 
-   ! declare IO process configuration inside adios
-   call adios2_declare_io(io_write, adios, "test_io_write", ierr)
+    ! read data (rank 0 only)
+    if (irank == 0) then
+        call reader%init_adios2(MPI_COMM_SELF, "test_io_read")
+        call reader%open_adios2("test_output.bp", adios2_mode_read)
+        call reader%begin_step_adios2()
 
-   ! define adios2 variable to be written in bp format
-   call adios2_define_variable(var, io_write, "data2D", adios2_type_real, 2, &
-                               shape_dims, start_dims, count_dims, &
-                               adios2_constant_dims, ierr)
+        sel_start = [0, 0]
+        sel_count = [shape_dims(1), shape_dims(2)]
 
-   ! open in write mode, this launches an engine
-   call adios2_open(engine, io_write, "test_output.bp", adios2_mode_write, ierr)
+        ! read entire dataset
+        allocate(data_read(sel_count(1), sel_count(2)))
+        call reader%read_data("data2D", data_read, sel_start, sel_count)
 
-   call adios2_begin_step(engine, ierr)
-
-   ! put data content to bp buffer
-   call adios2_put(engine, var, data_write, adios2_mode_sync, ierr)
-   call adios2_end_step(engine, ierr)
-
-   ! closes engine1 and deallocates it, becomes unreachable
-   call adios2_close(engine, ierr)
-   if (allocated(data_write)) deallocate (data_write)
-
-   ! reading back the data (only rank 0)
-   if (irank == 0) then
-      call adios2_init(adios, MPI_COMM_SELF, ierr)
-      call adios2_declare_io(io_read, adios, "test_io_read", ierr)
-
-      ! open in read mode, this launches an engine
-      call adios2_open(engine, io_read, "test_output.bp", adios2_mode_read, ierr)
-
-      call adios2_begin_step(engine, ierr)
-      call adios2_inquire_variable(var, io_read, "data2D", ierr)
-
-      if (ierr == adios2_found) then
-
-         sel_start = [0, 0]
-         sel_count = [shape_dims(1), shape_dims(2)]
-
-         ! select entire dataset
-         allocate (data_read(sel_count(1), sel_count(2)))
-         call adios2_set_selection(var, 2, sel_start, sel_count, ierr)
-         call adios2_get(engine, var, data_read, adios2_mode_sync, ierr)
-
-         ! data verification
-         do j = 1, sel_count(2)
+        ! verify data
+        do j = 1, sel_count(2)
             do i = 1, sel_count(1)
-               if (abs(data_read(i, j) - real((j - 1)*shape_dims(1) + (i - 1), kind=8)) > 1.e-8_dp) then
-                  allpass = .false.
-                  print *, "Data mismatch at (", i, ",", j, "): ", data_read(i, j)
-               end if
+                ! calculate expected value based on original data pattern
+                rank_id = (i-1) / inx ! determine which rank wrote this data
+                expected = rank_id*inx*iny + (j-1)*inx + mod(i-1,inx)
+                if (abs(data_read(i,j) - expected ) > 1.e-8_dp) then
+                    allpass = .false.
+                    print *, "Data mismatch at (", i, ",", j, "): ", data_read(i,j)
+                end if
             end do
-         end do
+        end do
 
-         if (allocated(data_read)) deallocate (data_read)
-      end if
+        call reader%end_step_adios2()
+        call reader%close_adios2()
 
-      call adios2_end_step(engine, ierr)
-      call adios2_close(engine, ierr)
-   end if
+        if (allocated(data_read)) deallocate(data_read)
+    end if
 
-   ! cleanup
-   call adios2_finalize(adios, ierr)
-   call MPI_Barrier(MPI_COMM_WORLD, ierr)
-   call MPI_Finalize(ierr)
+    ! Cleanup and finalize
+    call MPI_Barrier(MPI_COMM_WORLD, ierr)
+    call MPI_Finalize(ierr)
 
-   ! test result
-   if (irank == 0) then
-      if (allpass) then
-         write (stderr, '(a)') 'ADIOS2 TEST PASSED SUCCESSFULLY.'
-      else
-         error stop 'ADIOS2 TEST FAILED.'
-      end if
-   end if
+    ! Test result
+    if (irank == 0) then
+        if (allpass) then
+            write(stderr,'(a)') 'ADIOS2 TEST PASSED SUCCESSFULLY.'
+        else
+            error stop 'ADIOS2 TEST FAILED.'
+        end if
+    end if
 
 end program test_adios2
