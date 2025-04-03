81e6260e73aa512358a60fbd7c3ff1b355361131
Paul Bartholomew <p.bartholomew@epcc.ed.ac.uk>
2025-04-03 15:51:29 +0100

    Specify conformable array assignments
    This was identified by (amd)flang compiler. Testing similar code
    with crayftn also gives a compiletime error, gfortran does not
    warn.

--- tests/test_reorder_map.f90
+++ tests/test_reorder_map.f90
@@ -2,40 +2,40 @@ program test_reorder_map
   !! Check that reorder mappings return the expected values
 
   use m_common
-  
+
   implicit none
 
   logical :: test_pass
 
   test_pass = .true.
-  
+
   call test_get_dirs_from_rdr()
   call test_get_rdr_from_dirs()
 
   if (.not. test_pass) then
     error stop "FAIL"
   end if
-  
+
 contains
 
   subroutine test_get_dirs_from_rdr()
-    
+
     call test_one_get_dirs_from_rdr("RDR_X2Y", RDR_X2Y, DIR_X, DIR_Y)
     call test_one_get_dirs_from_rdr("RDR_X2Z", RDR_X2Z, DIR_X, DIR_Z)
     call test_one_get_dirs_from_rdr("RDR_X2C", RDR_X2C, DIR_X, DIR_C)
-    
+
     call test_one_get_dirs_from_rdr("RDR_Y2X", RDR_Y2X, DIR_Y, DIR_X)
     call test_one_get_dirs_from_rdr("RDR_Y2Z", RDR_Y2Z, DIR_Y, DIR_Z)
     call test_one_get_dirs_from_rdr("RDR_Y2C", RDR_Y2C, DIR_Y, DIR_C)
-    
+
     call test_one_get_dirs_from_rdr("RDR_Z2X", RDR_Z2X, DIR_Z, DIR_X)
     call test_one_get_dirs_from_rdr("RDR_Z2Y", RDR_Z2Y, DIR_Z, DIR_Y)
     call test_one_get_dirs_from_rdr("RDR_Z2C", RDR_Z2C, DIR_Z, DIR_C)
-    
+
     call test_one_get_dirs_from_rdr("RDR_C2X", RDR_C2X, DIR_C, DIR_X)
     call test_one_get_dirs_from_rdr("RDR_C2Y", RDR_C2Y, DIR_C, DIR_Y)
     call test_one_get_dirs_from_rdr("RDR_C2Z", RDR_C2Z, DIR_C, DIR_Z)
-    
+
   end subroutine test_get_dirs_from_rdr
 
   subroutine test_one_get_dirs_from_rdr(test, rdr, expect_from, expect_to)
@@ -71,7 +71,7 @@ contains
     call test_one_get_rdr_from_dirs("C->X", DIR_C, DIR_X, RDR_C2X)
     call test_one_get_rdr_from_dirs("C->Y", DIR_C, DIR_Y, RDR_C2Y)
     call test_one_get_rdr_from_dirs("C->Z", DIR_C, DIR_Z, RDR_C2Z)
-    
+
   end subroutine test_get_rdr_from_dirs
 
   subroutine test_one_get_rdr_from_dirs(test, from, to, expect_rdr)
@@ -89,5 +89,5 @@ contains
     end if
 
   end subroutine test_one_get_rdr_from_dirs
-  
+
 end program test_reorder_map
