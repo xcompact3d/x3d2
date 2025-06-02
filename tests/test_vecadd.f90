6e3db9a9988e56e945920bcf6de6e77bc3b1a2cd
Paul Bartholomew <p.bartholomew@epcc.ed.ac.uk>
2025-06-02 20:41:16 +0000

    Update test_vecadd to use host/device memory
    This now runs on OMP and CUDA

--- tests/test_vecadd.f90
+++ tests/test_vecadd.f90
@@ -29,16 +29,17 @@ program test_vecadd
   class(base_backend_t), pointer :: backend => null()
 #ifdef CUDA
   type(cuda_allocator_t), target :: cuda_allocator
+  type(allocator_t), target :: host_allocator
   type(cuda_backend_t), target :: cuda_backend
 #else
   type(allocator_t), target :: omp_allocator
+  type(allocator_t), pointer :: host_allocator
   type(omp_backend_t), target :: omp_backend
 #endif
   class(field_t), pointer :: a => null()
   class(field_t), pointer :: b => null()
   class(field_t), pointer :: c => null()
   class(field_t), pointer :: z => null()
-  class(field_t), pointer :: r => null()
 
   integer, dimension(3) :: dims_global
   integer, dimension(3) :: nproc_dir
@@ -63,11 +64,15 @@ program test_vecadd
 #ifdef CUDA
   cuda_allocator = cuda_allocator_t(mesh, SZ)
   allocator => cuda_allocator
+  host_allocator = allocator_t(mesh, SZ)
+
   cuda_backend = cuda_backend_t(mesh, allocator)
   backend => cuda_backend
 #else
   omp_allocator = allocator_t(mesh, SZ)
   allocator => omp_allocator
+  host_allocator => omp_allocator
+
   omp_backend = omp_backend_t(mesh, allocator)
   backend => omp_backend
 #endif
@@ -76,39 +81,12 @@ program test_vecadd
 
   do d = 1, size(dirs)
 
-    call initialise_data(a, b, c, z, r, d)
+    call initialise_data(a, b, c, z, d)
     
-    ! a + 0 = a
-    r%data = z%data
-    call backend%vecadd(1.0_dp, a, 1.0_dp, r)
-    if (any(r%data /= a%data)) then
-      print *, dirnames(d), ": a + 0 = a FAILED"
-      test_pass = .false.
-    end if
-    
-    ! a + a = 2a
-    r%data = a%data
-    call backend%vecadd(1.0_dp, a, 1.0_dp, r)
-    if (any(r%data /= 2 * a%data)) then
-      print *, dirnames(d), ": a + a = 2a FAILED"
-      test_pass = .false.
-    end if
-
-    ! a + (-a) = 0
-    r%data = -a%data
-    call backend%vecadd(1.0_dp, a, 1.0_dp, r)
-    if (any(r%data /= 0.0_dp)) then
-      print *, dirnames(d), ": a + (-a) = 0 FAILED"
-      test_pass = .false.
-    end if
-    
-    ! a + b = c
-    r%data = b%data
-    call backend%vecadd(1.0_dp, a, 1.0_dp, r)
-    if (any(r%data /= c%data)) then
-      print *, dirnames(d), ": a + b = c FAILED"
-      test_pass = .false.
-    end if
+    call test_add_zero(d)
+    call test_double(d)
+    call test_subself(d)
+    call test_addab(d)
 
   end do
 
@@ -116,7 +94,6 @@ program test_vecadd
   call allocator%release_block(b)
   call allocator%release_block(c)
   call allocator%release_block(z)
-  call allocator%release_block(r)
   
   if (test_pass) then
     print *, "PASS"
@@ -128,10 +105,130 @@ program test_vecadd
 
 contains
 
-  subroutine initialise_data(a, b, c, z, r, d)
+  subroutine test_add_zero(d)
 
     integer, intent(in) :: d
-    class(field_t), pointer, intent(inout) :: a, b, c, z, r
+
+    class(field_t), pointer :: r
+    class(field_t), pointer :: r_host, a_host
+
+    r => allocator%get_block(dirs(d))
+    r_host => host_allocator%get_block(dirs(d))
+    a_host => host_allocator%get_block(dirs(d))
+
+    call backend%get_field_data(r_host%data, z, dirs(d))
+    call backend%set_field_data(r, r_host%data, dirs(d))
+
+    call backend%vecadd(1.0_dp, a, 1.0_dp, r)
+
+    call backend%get_field_data(a_host%data, a, dirs(d))
+    call backend%get_field_data(r_host%data, r, dirs(d))
+    if (any(r_host%data /= a_host%data)) then
+      print *, dirnames(d), ": a + 0 = a failed"
+      test_pass = .false.
+    end if
+
+    call allocator%release_block(r)
+    call host_allocator%release_block(r_host)
+    call host_allocator%release_block(a_host)
+
+  end subroutine
+
+  subroutine test_double(d)
+
+    integer, intent(in) :: d
+
+    class(field_t), pointer :: r
+    class(field_t), pointer :: r_host, a_host
+
+    r => allocator%get_block(dirs(d))
+    r_host => host_allocator%get_block(dirs(d))
+    a_host => host_allocator%get_block(dirs(d))
+
+    call backend%get_field_data(a_host%data, a, dirs(d))
+    r_host%data = a_host%data
+    call backend%set_field_data(r, r_host%data, dirs(d))
+
+    call backend%vecadd(1.0_dp, a, 1.0_dp, r)
+
+    call backend%get_field_data(r_host%data, r, dirs(d))
+    if (any(r_host%data /= 2 * a_host%data)) then
+      print *, dirnames(d), ": a + a = 2a failed"
+      test_pass = .false.
+    end if
+
+    call allocator%release_block(r)
+    call host_allocator%release_block(r_host)
+    call host_allocator%release_block(a_host)
+
+  end subroutine
+
+  subroutine test_subself(d)
+
+    integer, intent(in) :: d
+
+    class(field_t), pointer :: r
+    class(field_t), pointer :: r_host, a_host
+
+    r => allocator%get_block(dirs(d))
+    r_host => host_allocator%get_block(dirs(d))
+    a_host => host_allocator%get_block(dirs(d))
+
+    call backend%get_field_data(a_host%data, a, dirs(d))
+    r_host%data = -a_host%data
+    call backend%set_field_data(r, r_host%data, dirs(d))
+
+    call backend%vecadd(1.0_dp, a, 1.0_dp, r)
+
+    call backend%get_field_data(r_host%data, r, dirs(d))
+    if (any(r_host%data /= 0)) then
+      print *, dirnames(d), ": a + (-a) = 0 failed"
+      test_pass = .false.
+    end if
+
+    call allocator%release_block(r)
+    call host_allocator%release_block(r_host)
+    call host_allocator%release_block(a_host)
+
+  end subroutine
+
+  subroutine test_addab(d)
+
+    integer, intent(in) :: d
+
+    class(field_t), pointer :: r
+    class(field_t), pointer :: r_host, b_host, c_host
+
+    r => allocator%get_block(dirs(d))
+    r_host => host_allocator%get_block(dirs(d))
+    b_host => host_allocator%get_block(dirs(d))
+    c_host => host_allocator%get_block(dirs(d))
+
+    call backend%get_field_data(b_host%data, b, dirs(d))
+    r_host%data = b_host%data
+    call backend%set_field_data(r, r_host%data, dirs(d))
+
+    call backend%vecadd(1.0_dp, a, 1.0_dp, r)
+
+    call backend%get_field_data(r_host%data, r, dirs(d))
+    call backend%get_field_data(c_host%data, c, dirs(d))
+    if (any(r_host%data /= c_host%data)) then
+      print *, dirnames(d), ": a + b = c failed"
+      test_pass = .false.
+    end if
+
+    call allocator%release_block(r)
+    call host_allocator%release_block(r_host)
+    call host_allocator%release_block(b_host)
+    call host_allocator%release_block(c_host)
+
+  end subroutine
+
+  subroutine initialise_data(a, b, c, z, d)
+
+    integer, intent(in) :: d
+    class(field_t), pointer, intent(inout) :: a, b, c, z
+    class(field_t), pointer :: a_host, b_host, c_host, z_host
 
     if (associated(a)) then
       call allocator%release_block(a)
@@ -145,21 +242,32 @@ contains
     if (associated(z)) then
       call allocator%release_block(z)
     end if
-    if (associated(r)) then
-      call allocator%release_block(r)
-    end if
 
     a => allocator%get_block(dirs(d))
     b => allocator%get_block(dirs(d))
     c => allocator%get_block(dirs(d))
     z => allocator%get_block(dirs(d))
-    r => allocator%get_block(dirs(d))
+
+    a_host => host_allocator%get_block(dirs(d))
+    b_host => host_allocator%get_block(dirs(d))
+    c_host => host_allocator%get_block(dirs(d))
+    z_host => host_allocator%get_block(dirs(d))
 
     ! Initialise values
-    call random_number(a%data)
-    call random_number(b%data)
-    c%data = a%data + b%data
-    z%data = 0.0_dp
+    call random_number(a_host%data)
+    call random_number(b_host%data)
+    c_host%data = a_host%data + b_host%data
+    z_host%data = 0.0_dp
+
+    call backend%set_field_data(a, a_host%data)
+    call backend%set_field_data(b, b_host%data)
+    call backend%set_field_data(c, c_host%data)
+    call backend%set_field_data(z, z_host%data)
+
+    call host_allocator%release_block(a_host)
+    call host_allocator%release_block(b_host)
+    call host_allocator%release_block(c_host)
+    call host_allocator%release_block(z_host)
 
   end subroutine initialise_data
   
