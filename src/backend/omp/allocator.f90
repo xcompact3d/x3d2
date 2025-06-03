47eef8b6f52d7fdc704af1f684f883c1b4ccc2e6
Paul Bartholomew <p.bartholomew@epcc.ed.ac.uk>
2025-06-03 12:01:44 +0100

    WIP: Implement a OpenMP target field type and allocator

---
+++ src/backend/omp/allocator.f90
@@ -0,0 +1,85 @@
+!!! backends/omp/allocator.f90
+!!
+!! Implements an allocator specialised to OMP target offloading
+
+module m_omptgt_allocator
+
+  use m_common, only: dp
+
+  use m_allocator, only: allocator_t
+  use m_mesh, only: mesh_t
+  use m_field, only: field_t
+
+  implicit none
+
+  private
+  public :: omptgt_allocator_t
+  public :: omptgt_field_t
+
+  type, extends(allocator_t) :: omptgt_allocator_t
+  contains
+    procedure :: create_block => create_block_omptgt
+  end type omptgt_allocator_t
+
+  interface omptgt_allocator_t
+    module procedure omptgt_allocator_init
+  end interface omptgt_allocator_t
+
+  type, extends(field_t) :: omptgt_field_t
+    ! A device-resident field
+  contains
+    procedure :: fill => fill_omptgt
+  end type omptgt_field_t
+  
+  interface omptgt_field_t
+    module procedure omptgt_field_init
+  end interface omptgt_field_t
+  
+contains
+
+  ! Constructor for the OMP target offload allocator
+  type(omptgt_allocator_t) function omptgt_allocator_init(mesh, sz) result(a)
+    type(mesh_t), intent(inout) :: mesh
+    integer, intent(in) :: sz
+
+    a%allocator_t = allocator_t(mesh, sz)
+  end function omptgt_allocator_init
+
+  ! Allocates a device-resident block
+  class(field_t), pointer function create_block_omptgt(self, next) result(ptr)
+    class(omptgt_allocator_t) :: self
+    type(omptgt_field_t), pointer, intent(in) :: next
+
+    allocate(omptgt_field_t :: ptr)
+    self%next_id = self%next_id + 1
+    ptr = omptgt_field_t(self%ngrid, next, id=self%next_if)
+
+  end function create_block_omptgt
+
+  ! Constructs a device-resident field
+  type(omptgt_field_t) function omptgt_field_init(ngrid, next, id) result(f)
+    integer, intent(in) :: ngrid
+    type(omptgt_field_t), pointer, intent(in) :: next
+    integer, intent(in) :: id
+
+    f%field_t = field_t(ngrid, next, id)
+    !$omp target enter data map(allocate:f%p_data) map(to:f%refcount) map(to:f%id)
+    
+  end function omptgt_field_init
+
+  subroutine fill_omptgt(self, c)
+    class(omptgt_field_t) :: self
+    real(dp), intent(in) :: c
+
+    integer :: i
+    
+    !$omp target parallel teams distribute parallel do
+    do i = 1, size(self%p_data)
+      self%p_data(i) = c
+    end do
+    !$omp end target parallel teams distribute parallel do
+
+  end subroutine fill_omptgt
+
+end module m_omptgt_allocator
+  
