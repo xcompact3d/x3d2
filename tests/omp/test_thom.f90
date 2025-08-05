158511ec30609bc5dbf5a80925090b97fc761cf8
Paul Bartholomew <p.bartholomew@epcc.ed.ac.uk>
2025-08-05 09:39:44 +0100

    Fix indentation problems

--- tests/omp/test_thom.f90
+++ tests/omp/test_thom.f90
@@ -5,9 +5,9 @@ program test_thom
   use m_omp_common, only: SZ
   use m_tdsops, only: tdsops_t, tdsops_init
   use m_exec_thom, only: exec_thom_tds_compact
-  
+
   implicit none
-  
+
   integer :: n_glob, n, n_groups
   integer :: n_iters
   integer :: ndof
@@ -24,7 +24,7 @@ program test_thom
   logical :: allpass
 
   allpass = .true.
-  
+
   !! Performance test
   ! n_glob = 512
   ! n_groups = 512 * 512 / SZ
@@ -32,16 +32,16 @@ program test_thom
 
   !! Verification test
   n_glob = 1024
-  n_groups = 64 * 64 / SZ
+  n_groups = 64*64/SZ
   n_iters = 1
 
   n = n_glob
-  ndof = n_glob * n_groups * SZ
+  ndof = n_glob*n_groups*SZ
 
-  allocate(u(SZ, n, n_groups), du(SZ, n, n_groups))
+  allocate (u(SZ, n, n_groups), du(SZ, n, n_groups))
 
-  dx_per = 2 * pi / n_glob
-  dx = 2 * pi / (n_glob - 1)
+  dx_per = 2*pi/n_glob
+  dx = 2*pi/(n_glob - 1)
 
   !! Periodic case
   print *, "=== Testing periodic case ==="
@@ -49,14 +49,14 @@ program test_thom
   do k = 1, n_groups
     do j = 1, n
       do i = 1, SZ
-        u(i, j, k) = sin((j - 1) * dx_per)
+        u(i, j, k) = sin((j - 1)*dx_per)
       end do
     end do
   end do
 
   tdsops = tdsops_init(n, dx_per, &
-                       operation = "second-deriv", scheme = "compact6", &
-                       bc_start = BC_PERIODIC, bc_end = BC_PERIODIC)
+                       operation="second-deriv", scheme="compact6", &
+                       bc_start=BC_PERIODIC, bc_end=BC_PERIODIC)
 
   tstart = omp_get_wtime()
   do i = 1, n_iters
@@ -70,18 +70,18 @@ program test_thom
 
   !! Dirichlet case
   print *, "=== Testing Dirichlet case ==="
-  
+
   do k = 1, n_groups
     do j = 1, n
       do i = 1, SZ
-        u(i, j, k) = sin((j - 1) * dx)
+        u(i, j, k) = sin((j - 1)*dx)
       end do
     end do
   end do
 
   tdsops = tdsops_init(n, dx, &
-                       operation = "second-deriv", scheme = "compact6", &
-                       bc_start = BC_DIRICHLET, bc_end = BC_DIRICHLET)
+                       operation="second-deriv", scheme="compact6", &
+                       bc_start=BC_DIRICHLET, bc_end=BC_DIRICHLET)
 
   tstart = omp_get_wtime()
   do i = 1, n_iters
@@ -112,40 +112,40 @@ contains
     else
       nbytes = 4_dp
     end if
-    achievedBW = consumed_bw * n_iters * ndof * nbytes / trun
+    achievedBW = consumed_bw*n_iters*ndof*nbytes/trun
 
-    print *, "Achieved BW: ", achievedBW / (2**30), " GiB/s"
+    print *, "Achieved BW: ", achievedBW/(2**30), " GiB/s"
 
     memClockRt = 3200
     memBusWidth = 64
-    deviceBW = 2 * memBusWidth / nbytes * memClockRt * (10**6)
+    deviceBW = 2*memBusWidth/nbytes*memClockRt*(10**6)
 
-    print *, "Available BW: ", deviceBW / 2**30, " GiB/s / NUMA zone on ARCHER2"
-    print *, "Utilised BW: ", 100 * (achievedBW / deviceBW), " %"
-    
+    print *, "Available BW: ", deviceBW/2**30, " GiB/s / NUMA zone on ARCHER2"
+    print *, "Utilised BW: ", 100*(achievedBW/deviceBW), " %"
+
   end subroutine checkperf
 
   subroutine checkerr(u, du, tol)
 
     real(dp), dimension(:, :, :), intent(in) :: u, du
     real(dp), intent(in) :: tol
-    
+
     real(dp) :: norm_du
 
-    norm_du = sum((u + du)**2) / n_glob / n_groups / SZ
+    norm_du = sum((u + du)**2)/n_glob/n_groups/SZ
     norm_du = sqrt(norm_du)
 
-    print *, minval(u + du), maxval(u+du)
-    
+    print *, minval(u + du), maxval(u + du)
+
     print *, "error norm", norm_du
 
     if (norm_du > tol) then
       print *, "Check second derivatives... FAILED"
       allpass = .false.
     else
-      print *,  "Check second derivatives... PASSED"
+      print *, "Check second derivatives... PASSED"
     end if
-    
+
   end subroutine checkerr
-  
+
 end program test_thom
--- tests/test_adios2_read_write.f90
+++ tests/test_adios2_read_write.f90
@@ -1,101 +1,103 @@
 program test_adios2
-    use mpi
-    use m_adios2_io, only : adios2_writer_t, adios2_reader_t, &
-                            adios2_mode_write, adios2_mode_read, &
-                            adios2_file_t
-    use m_common, only: dp
-    use iso_fortran_env, only: int64, real64, stderr => error_unit
-    implicit none
+  use mpi
+  use m_adios2_io, only: adios2_writer_t, adios2_reader_t, &
+                         adios2_mode_write, adios2_mode_read, &
+                         adios2_file_t
+  use m_common, only: dp
+  use iso_fortran_env, only: int64, real64, stderr => error_unit
+  implicit none
 
-    ! ADIOS2 handlers
-    type(adios2_writer_t) :: adios2_writer
-    type(adios2_reader_t) :: adios2_reader
-    type(adios2_file_t) :: file
+  ! ADIOS2 handlers
+  type(adios2_writer_t) :: adios2_writer
+  type(adios2_reader_t) :: adios2_reader
+  type(adios2_file_t) :: file
 
-    ! MPI variables
-    integer :: ierr, irank, isize
-    integer(kind=int64), dimension(2) :: shape_dims, start_dims, count_dims
-    integer(kind=int64), dimension(2) :: sel_start, sel_count
-    real(kind=real64), dimension(:,:), allocatable :: data_write, data_read
+  ! MPI variables
+  integer :: ierr, irank, isize
+  integer(kind=int64), dimension(2) :: shape_dims, start_dims, count_dims
+  integer(kind=int64), dimension(2) :: sel_start, sel_count
+  real(kind=real64), dimension(:, :), allocatable :: data_write, data_read
 
-    ! application variables
-    integer :: i, j, rank_id, inx = 3, iny = 4
-    logical :: allpass = .true.
-    real(kind=real64) :: expected
+  ! application variables
+  integer :: i, j, rank_id, inx = 3, iny = 4
+  logical :: allpass = .true.
+  real(kind=real64) :: expected
 
-    ! launch MPI
-    call MPI_Init(ierr)
-    call MPI_Comm_rank(MPI_COMM_WORLD, irank, ierr)
-    call MPI_Comm_size(MPI_COMM_WORLD, isize, ierr)
+  ! launch MPI
+  call MPI_Init(ierr)
+  call MPI_Comm_rank(MPI_COMM_WORLD, irank, ierr)
+  call MPI_Comm_size(MPI_COMM_WORLD, isize, ierr)
 
-    ! data initialization
-    allocate(data_write(inx, iny))
+  ! data initialization
+  allocate (data_write(inx, iny))
 
-    ! initialize data (unique per rank)
-    do j = 1, iny
-        do i = 1, inx
-            data_write(i,j) = irank*inx*iny + (j-1)*inx + (i-1)
-        end do
+  ! initialize data (unique per rank)
+  do j = 1, iny
+    do i = 1, inx
+      data_write(i, j) = irank*inx*iny + (j - 1)*inx + (i - 1)
+    end do
+  end do
+
+  ! global shape and local offsets for parallel I/O
+  shape_dims = [isize*inx, iny]
+  start_dims = [irank*inx, 0]
+  count_dims = [inx, iny]
+
+  ! write data
+  call adios2_writer%init(MPI_COMM_WORLD, "test_io_write")
+  file = adios2_writer%open("test_output.bp", adios2_mode_write)
+  call adios2_writer%begin_step(file)
+  call adios2_writer%write_data("data2D", data_write, file, &
+                                shape_dims, start_dims, count_dims)
+  call adios2_writer%end_step(file)
+  call adios2_writer%close(file)
+
+  if (allocated(data_write)) deallocate (data_write)
+
+  ! read data (rank 0 only)
+  if (irank == 0) then
+    call adios2_reader%init(MPI_COMM_SELF, "test_io_read")
+    file = adios2_reader%open("test_output.bp", adios2_mode_read)
+    call adios2_reader%begin_step(file)
+
+    sel_start = [0, 0]
+    sel_count = [shape_dims(1), shape_dims(2)]
+
+    ! read entire dataset
+    allocate (data_read(sel_count(1), sel_count(2)))
+    call adios2_reader%read_data("data2D", data_read, file, &
+                                 sel_start, sel_count)
+
+    ! verify data
+    do j = 1, sel_count(2)
+      do i = 1, sel_count(1)
+        ! calculate expected value based on original data pattern
+        rank_id = (i - 1)/inx ! determine which rank wrote this data
+        expected = rank_id*inx*iny + (j - 1)*inx + mod(i - 1, inx)
+        if (abs(data_read(i, j) - expected) > 1.e-8_dp) then
+          allpass = .false.
+          print *, "Data mismatch at (", i, ",", j, "): ", data_read(i, j)
+        end if
+      end do
     end do
 
-    ! global shape and local offsets for parallel I/O
-    shape_dims = [isize*inx, iny]
-    start_dims = [irank*inx, 0]
-    count_dims = [inx, iny]
+    call adios2_reader%end_step(file)
+    call adios2_reader%close(file)
 
-    ! write data
-    call adios2_writer%init(MPI_COMM_WORLD, "test_io_write")
-    file = adios2_writer%open("test_output.bp", adios2_mode_write)
-    call adios2_writer%begin_step(file)
-    call adios2_writer%write_data("data2D", data_write, file, shape_dims, start_dims, count_dims)
-    call adios2_writer%end_step(file)
-    call adios2_writer%close(file)
+    if (allocated(data_read)) deallocate (data_read)
+  end if
 
-    if (allocated(data_write)) deallocate(data_write)
+  ! Cleanup and finalize
+  call MPI_Barrier(MPI_COMM_WORLD, ierr)
+  call MPI_Finalize(ierr)
 
-    ! read data (rank 0 only)
-    if (irank == 0) then
-        call adios2_reader%init(MPI_COMM_SELF, "test_io_read")
-        file = adios2_reader%open("test_output.bp", adios2_mode_read)
-        call adios2_reader%begin_step(file)
-
-        sel_start = [0, 0]
-        sel_count = [shape_dims(1), shape_dims(2)]
-
-        ! read entire dataset
-        allocate(data_read(sel_count(1), sel_count(2)))
-        call adios2_reader%read_data("data2D", data_read, file, sel_start, sel_count)
-
-        ! verify data
-        do j = 1, sel_count(2)
-            do i = 1, sel_count(1)
-                ! calculate expected value based on original data pattern
-                rank_id = (i-1) / inx ! determine which rank wrote this data
-                expected = rank_id*inx*iny + (j-1)*inx + mod(i-1,inx)
-                if (abs(data_read(i,j) - expected ) > 1.e-8_dp) then
-                    allpass = .false.
-                    print *, "Data mismatch at (", i, ",", j, "): ", data_read(i,j)
-                end if
-            end do
-        end do
-
-        call adios2_reader%end_step(file)
-        call adios2_reader%close(file)
-
-        if (allocated(data_read)) deallocate(data_read)
+  ! Test result
+  if (irank == 0) then
+    if (allpass) then
+      write (stderr, '(a)') 'ADIOS2 TEST PASSED SUCCESSFULLY.'
+    else
+      error stop 'ADIOS2 TEST FAILED.'
     end if
-
-    ! Cleanup and finalize
-    call MPI_Barrier(MPI_COMM_WORLD, ierr)
-    call MPI_Finalize(ierr)
-
-    ! Test result
-    if (irank == 0) then
-        if (allpass) then
-            write(stderr,'(a)') 'ADIOS2 TEST PASSED SUCCESSFULLY.'
-        else
-            error stop 'ADIOS2 TEST FAILED.'
-        end if
-    end if
+  end if
 
 end program test_adios2
