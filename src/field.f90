2225990413c39045c69b7d9a80303f87c615cd78
Irufan Ahmed <irufan.ahmed04@imperial.ac.uk>
2025-12-10 17:55:42 +0000

    add comments to src files

--- src/field.f90
+++ src/field.f90
@@ -1,25 +1,32 @@
 module m_field
+  !! Field data structure module for managing computational grid data.
+  !!
+  !! This module provides the field_t type for storing 3D scalar fields
+  !! on the computational grid. Fields can be organised in linked lists
+  !! for memory management and support different data orientations
+  !! (x-pencil, y-pencil, z-pencil).
 
   use m_common, only: dp, DIR_X, DIR_Y, DIR_Z, DIR_C
 
   type :: field_t
-     !! Memory block type holding both a data field and a pointer
-     !! to the next block.  The `field_t` type also holds a integer
-     !! `refcount` that counts the number of references to this
-     !! field.  User code is currently responsible for incrementing
-     !! the reference count.
-    class(field_t), pointer :: next
-    real(dp), pointer, private :: p_data(:)
-    real(dp), pointer, contiguous :: data(:, :, :)
-    integer :: dir
-    integer :: data_loc
-    integer :: refcount = 0
-    integer :: id !! An integer identifying the memory block.
+    !! Memory block type holding a 3D scalar field with metadata.
+    !!
+    !! The field_t type stores both a data field and a pointer to the next
+    !! block, enabling linked list structures for memory management. The type
+    !! tracks a reference count (currently managed by user code), data
+    !! orientation (x-, y-, or z-pencil), and data location on the staggered grid.
+    class(field_t), pointer :: next             !! Pointer to next field in linked list
+    real(dp), pointer, private :: p_data(:)     !! 1D array storage for data
+    real(dp), pointer, contiguous :: data(:, :, :)  !! 3D view of data array
+    integer :: dir                              !! Data direction (DIR_X, DIR_Y, DIR_Z, or DIR_C)
+    integer :: data_loc                         !! Data location flag (VERT, CELL, etc.)
+    integer :: refcount = 0                     !! Reference count for memory management
+    integer :: id                               !! Unique identifier for this memory block
   contains
-    procedure :: fill
-    procedure :: get_shape
-    procedure :: set_shape
-    procedure :: set_data_loc
+    procedure :: fill          !! Fill field with a constant value
+    procedure :: get_shape     !! Get 3D dimensions of data array
+    procedure :: set_shape     !! Set 3D dimensions by reshaping p_data
+    procedure :: set_data_loc  !! Set data location flag
   end type field_t
 
   interface field_t
@@ -27,16 +34,25 @@ module m_field
   end interface field_t
 
   type :: flist_t
-    !! Use for creating a list of field pointers
-    class(field_t), pointer :: ptr
+    !! Wrapper type for creating arrays of field pointers.
+    !!
+    !! This type is used to create lists or arrays of field pointers,
+    !! useful for managing multiple fields such as velocity components
+    !! or transported scalar species.
+    class(field_t), pointer :: ptr  !! Pointer to a field
   end type flist_t
 
 contains
 
   function field_init(ngrid, next, id) result(f)
-    integer, intent(in) :: ngrid, id
-    type(field_t), pointer, intent(in) :: next
-    type(field_t) :: f
+    !! Initialise a new field with allocated memory.
+    !!
+    !! Creates a new field_t instance with allocated storage for ngrid points.
+    !! The field is linked to the next field in the list and assigned a unique ID.
+    integer, intent(in) :: ngrid  !! Total number of grid points to allocate
+    type(field_t), pointer, intent(in) :: next  !! Pointer to next field in linked list
+    integer, intent(in) :: id     !! Unique identifier for this field
+    type(field_t) :: f            !! Initialised field
 
     allocate (f%p_data(ngrid))
     f%refcount = 0
@@ -45,38 +61,52 @@ contains
   end function field_init
 
   subroutine fill(self, c)
+    !! Fill the entire field with a constant value.
+    !!
+    !! Sets all grid points in the field to the specified constant value.
     implicit none
 
-    class(field_t) :: self
-    real(dp), intent(in) :: c
+    class(field_t) :: self        !! Field to fill
+    real(dp), intent(in) :: c     !! Constant value to fill with
 
     self%p_data(:) = c
 
   end subroutine fill
 
   subroutine set_data_loc(self, data_loc)
-    class(field_t) :: self
-    integer, intent(in) :: data_loc
+    !! Set the data location flag for this field.
+    !!
+    !! The data location specifies where on the staggered grid the data
+    !! is located (e.g., VERT, CELL, X_FACE, etc.).
+    class(field_t) :: self           !! Field to modify
+    integer, intent(in) :: data_loc  !! Data location flag
 
     self%data_loc = data_loc
 
   end subroutine
 
   function get_shape(self) result(dims)
+    !! Get the 3D dimensions of the field data.
+    !!
+    !! Returns the current shape of the 3D data array.
     implicit none
 
-    class(field_t) :: self
-    integer :: dims(3)
+    class(field_t) :: self  !! Field to query
+    integer :: dims(3)      !! Array dimensions [nx, ny, nz]
 
     dims = shape(self%data)
 
   end function get_shape
 
   subroutine set_shape(self, dims)
+    !! Reshape the field data to specified 3D dimensions.
+    !!
+    !! Maps the 1D storage array (p_data) to a 3D view with the specified
+    !! dimensions. The total size must match the allocated storage.
     implicit none
 
-    class(field_t) :: self
-    integer, intent(in) :: dims(3)
+    class(field_t) :: self        !! Field to reshape
+    integer, intent(in) :: dims(3)  !! Target dimensions [nx, ny, nz]
 
     self%data(1:dims(1), 1:dims(2), 1:dims(3)) => self%p_data
 
