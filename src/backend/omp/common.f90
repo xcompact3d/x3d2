ec19b0edfca5e20c18f2e94c156d1e47e9d37278
Jacques Xing <jxing1@imperial.ac.uk>
2026-09-09 15:18:25 +0100

    Set OMP SZ value based on architecture (NVIDA vs. AMD)

--- src/backend/omp/common.f90
+++ src/backend/omp/common.f90
@@ -1,6 +1,12 @@
 module m_omp_common
   implicit none
 
-  integer, parameter :: SZ = 16
+#if defined(OMP_TGT_AMD)
+  integer, parameter :: SZ = 64
+#elif defined(OMP_TGT_NVIDIA)
+  integer, parameter :: SZ = 32
+#else
+   integer, parameter :: SZ = 16
+#endif
 
 end module m_omp_common
