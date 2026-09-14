a8f53256220e550b71ae72be7d82625df83ac2ee
kaanolgu <k.olgu20@gmail.com>
2026-09-14 14:03:57 +0100

    fix maybe-uninitialized benchmark timers

--- src/io/adios2/io.f90
+++ src/io/adios2/io.f90
@@ -903,7 +903,7 @@ contains
                                "Error defining ADIOS2 scalar i8 variable")
       end if
 
-      if (file_handle%bench_enabled) t0_put = MPI_Wtime()
+      t0_put = MPI_Wtime()
       call nvtx_push_if_enabled("ADIOS2_Put")
       call adios2_put(file_handle%engine, var, value, adios2_mode_deferred, &
                       ierr)
@@ -940,7 +940,7 @@ contains
                                "Error defining ADIOS2 scalar integer variable")
       end if
 
-      if (file_handle%bench_enabled) t0_put = MPI_Wtime()
+      t0_put = MPI_Wtime()
       call nvtx_push_if_enabled("ADIOS2_Put")
       call adios2_put(file_handle%engine, var, value, adios2_mode_deferred, &
                       ierr)
@@ -993,7 +993,7 @@ contains
       if (convert_to_sp .and. .not. is_sp) then
         value_sp = real(value, sp)
         ! Use sync mode to ensure data is copied before value_sp goes out of scope
-        if (file_handle%bench_enabled) t0_put = MPI_Wtime()
+        t0_put = MPI_Wtime()
         call nvtx_push_if_enabled("ADIOS2_Put")
         call adios2_put(file_handle%engine, var, value_sp, adios2_mode_sync, &
                         ierr)
@@ -1005,7 +1005,7 @@ contains
         call self%handle_error(ierr, "Error writing ADIOS2 scalar &
                                      &single precision real data")
       else
-        if (file_handle%bench_enabled) t0_put = MPI_Wtime()
+        t0_put = MPI_Wtime()
         call nvtx_push_if_enabled("ADIOS2_Put")
         call adios2_put(file_handle%engine, var, value, &
                         adios2_mode_deferred, ierr)
@@ -1068,7 +1068,7 @@ contains
         allocate (array_sp(size(array, 1), size(array, 2), size(array, 3)))
         array_sp = real(array, sp)
         ! Use sync mode to ensure data is copied before buffer is deallocated
-        if (file_handle%bench_enabled) t0_put = MPI_Wtime()
+        t0_put = MPI_Wtime()
         call nvtx_push_if_enabled("ADIOS2_Put")
         call adios2_put(file_handle%engine, var, array_sp, adios2_mode_sync, &
                         ierr)
@@ -1082,7 +1082,7 @@ contains
         call self%handle_error(ierr, "Error writing ADIOS2 3D array &
                                      &single precision real data")
       else
-        if (file_handle%bench_enabled) t0_put = MPI_Wtime()
+        t0_put = MPI_Wtime()
         call nvtx_push_if_enabled("ADIOS2_Put")
         call adios2_put(file_handle%engine, var, array, &
                         adios2_mode_deferred, ierr)
@@ -1168,7 +1168,7 @@ contains
         devptr = c_devloc(array_sp)
         device_ptr = transfer(devptr, device_ptr)
 
-        if (file_handle%bench_enabled) t0_put = MPI_Wtime()
+        t0_put = MPI_Wtime()
         call nvtx_push_if_enabled("ADIOS2_Put")
         ierr = adios2_put_c(transfer(file_handle%engine%f2c, c_null_ptr), &
                             transfer(var%f2c, c_null_ptr), device_ptr, &
@@ -1185,7 +1185,7 @@ contains
         devptr = c_devloc(array)
         device_ptr = transfer(devptr, device_ptr)
 
-        if (file_handle%bench_enabled) t0_put = MPI_Wtime()
+        t0_put = MPI_Wtime()
         call nvtx_push_if_enabled("ADIOS2_Put")
         ierr = adios2_put_c(transfer(file_handle%engine%f2c, c_null_ptr), &
                             transfer(var%f2c, c_null_ptr), device_ptr, &
@@ -1272,7 +1272,7 @@ contains
     if (self%is_step_active) call self%end_step()
 
     if (self%engine%valid) then
-      if (self%is_writer .and. self%bench_enabled) t0_close = MPI_Wtime()
+      t0_close = MPI_Wtime()
       call adios2_close(self%engine, ierr)
       if (self%is_writer .and. self%bench_enabled) then
         self%bench_close_time = self%bench_close_time + &
@@ -1323,7 +1323,7 @@ contains
 
     if (.not. self%is_step_active) return
 
-    if (self%is_writer .and. self%bench_enabled) t0_end_step = MPI_Wtime()
+    t0_end_step = MPI_Wtime()
     if (self%is_writer) call nvtx_push_if_enabled("ADIOS2_EndStep")
     call adios2_end_step(self%engine, ierr)
     if (self%is_writer) call nvtx_pop_if_enabled()
